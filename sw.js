77e5fe33fb3ac532e888764143738498fab8b052
Sudarshana2002dilshan <sudarshana2002dilshan@gmail.com>
2026-07-06 14:53:40 +0530

    Fix display conflicts

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
+const CACHE_NAME = 'dilshan-gps-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('gps-store').then((cache) => cache.addAll(['/', '/index.html', '/style.css', '/script.js'])));
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
