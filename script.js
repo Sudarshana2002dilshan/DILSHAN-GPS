2292a10c45c9827a2cd4ab3095389daa9b53bd2c
Sudarshana2002dilshan <sudarshana2002dilshan@gmail.com>
2026-07-06 16:00:42 +0530

    Fix display conflicts

--- script.js
+++ script.js
@@ -4,29 +4,15 @@ function showTab(id) {
 }
 function toggleTheme() { document.body.classList.toggle('dark-mode'); }
 
-window.onload = () => {
-    document.getElementById('lat').innerText = localStorage.getItem('lastLat') || "---";
-    document.getElementById('lon').innerText = localStorage.getItem('lastLon') || "---";
-    document.getElementById('time').innerText = localStorage.getItem('lastTime') || "--:--:--";
-};
-
 navigator.geolocation.watchPosition((pos) => {
-    const lat = pos.coords.latitude.toFixed(4) + " N";
-    const lon = pos.coords.longitude.toFixed(4) + " E";
-    const time = new Date().toLocaleTimeString();
-    
     document.getElementById('speed').innerText = (pos.coords.speed * 3.6 || 0).toFixed(1);
-    document.getElementById('lat').innerText = lat;
-    document.getElementById('lon').innerText = lon;
-    document.getElementById('time').innerText = time;
-
-    localStorage.setItem('lastLat', lat);
-    localStorage.setItem('lastLon', lon);
-    localStorage.setItem('lastTime', time);
+    document.getElementById('lat').innerText = pos.coords.latitude.toFixed(4) + " N";
+    document.getElementById('lon').innerText = pos.coords.longitude.toFixed(4) + " E";
+    document.getElementById('time').innerText = new Date().toLocaleTimeString();
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=b55f6eb21b285249ea39c2d19af58d88&units=metric`)
     .then(r => r.json()).then(d => {
-        document.getElementById('wind-speed').innerText = (d.wind.speed * 3.6).toFixed(1) + " km/h";
+        document.getElementById('wind-speed').innerText = (d.wind.speed * 3.6).toFixed(1);
         const dirs = ["උතුරු", "ඊසාන", "නැගෙනහිර", "අග්නිදිග", "දකුණ", "නිරිත", "බටහිර", "වයඹ"];
         document.getElementById('wind-dir').innerText = dirs[Math.round(d.wind.deg / 45) % 8];
     });
