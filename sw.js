fc749cfc7c4a5ca569eb2e1b4ad19664eaf75bbd
Sudarshana2002dilshan <sudarshana2002dilshan@gmail.com>
2026-07-07 22:22:26 +0530

    Fix display conflicts

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'dilshan-gps-v3';
+// සෑම අප්ඩේට් එකකදීම මේ කැෂේ නම (v4, v5 ලෙස) මාරු කරන්න
+const CACHE_NAME = 'dilshan-gps-v4'; 
 const ASSETS = [
   './',
   './index.html',
@@ -9,14 +10,32 @@ const ASSETS = [
   './firebase-firestore.js'
 ];
 
+// ඇප් එකේ ෆයිල් ටික ෆෝන් එකේ සේව් කරගැනීම
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting()) // අලුත් කෝඩ් එකක් ආ සැනින් පරණ එක මඟහැර ක්‍රියාත්මක වීම
   );
 });
 
+// පරණ Cache දත්ත ස්වයංක්‍රීයවම මකා දැමීම
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Old Cache Automatically Deleted:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Offline අවස්ථාවලදී සේව් කරගත් ෆයිල් ලබාදීම
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
