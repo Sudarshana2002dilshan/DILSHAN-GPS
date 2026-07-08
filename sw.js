1ea50b06218e73b3cb304d57d18e8c679ccea794
Sudarshana2002dilshan <sudarshana2002dilshan@gmail.com>
2026-07-08 10:50:37 +0530

    Fix display conflicts

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// සෑම අප්ඩේට් එකකදීම මේ කැෂේ නම (v4, v5 ලෙස) මාරු කරන්න
-const CACHE_NAME = 'dilshan-gps-v6'; 
+const CACHE_NAME = 'dilshan-gps-v10'; // අලුත්ම කැෂේ අංකය
 const ASSETS = [
   './',
   './index.html',
@@ -10,23 +9,18 @@ const ASSETS = [
   './firebase-firestore.js'
 ];
 
-// ඇප් එකේ ෆයිල් ටික ෆෝන් එකේ සේව් කරගැනීම
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting()) // අලුත් කෝඩ් එකක් ආ සැනින් පරණ එක මඟහැර ක්‍රියාත්මක වීම
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
-// පරණ Cache දත්ත ස්වයංක්‍රීයවම මකා දැමීම
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('Old Cache Automatically Deleted:', key);
             return caches.delete(key);
           }
         })
@@ -35,11 +29,8 @@ self.addEventListener('activate', (e) => {
   );
 });
 
-// Offline අවස්ථාවලදී සේව් කරගත් ෆයිල් ලබාදීම
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
